3d432aed7ea8e2374e756d35a0f0c624440c700d
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-16 20:39:36 +0545

    CONTD: notifications props

--- src/Notification.tsx
+++ src/Notification.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 type Props = {
-  type: string;
+  type: 'info' | 'success' | 'warning' | 'error';
   message: string;
   title: React.ReactNode;
+  timeOut: number;
+  onClick: () => void;
+  onRequestHide: () => void;
 };
 
-export const Notification = ({ type, message, title }: Props) => {
+export const Notification = ({
+  type = 'info',
+  message,
+  title,
+  timeOut = 5000,
+  onClick,
+  onRequestHide,
+}: Props) => {
   // const className = classnames(['notification', `notification-${type}`]);
   const className = `notification notification-${type}`;
   title = title ? <h4 className="title">{title}</h4> : null;

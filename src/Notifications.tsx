fdf3c02e87fd857679605eee76ae73d7f888a3f3
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-17 09:17:29 +0545

    Props added for notificaitons component

--- src/Notifications.tsx
+++ src/Notifications.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Notification } from './Notification';
 
-export const Notifications = () => {
+type Props = {
+  notifications: Notification[];
+  onRequestHide: () => void;
+  enterTimeout: number;
+  leaveTimeout: number;
+};
+
+export const Notifications = ({
+  notifications = [],
+  onRequestHide,
+  enterTimeout = 400,
+  leaveTimeout = 400,
+}: Props) => {
   return <></>;
 };

9a95010ad981d34fbc0162a1d1538d617a2472cb
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-17 09:28:25 +0545

    Props for Notification Container

--- src/NotificationContainer.tsx
+++ src/NotificationContainer.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+type Props = {
+  enterTimeout: number;
+  leaveTimeout: number;
+};
 
-export const NotificationContainer = () => {
+export const NotificationContainer = ({
+  enterTimeout = 400,
+  leaveTimeout = 400,
+}: Props) => {
   return <></>;
 };

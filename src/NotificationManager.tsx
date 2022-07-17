bc359119568ac1c98b00dcdf1fc05fab50aae8b8
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-17 09:41:11 +0545

    Notification Manager continue

--- src/NotificationManager.tsx
+++ src/NotificationManager.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import { EventEmitter } from 'events';
+import { useState } from 'react';
+import { INotificationType } from './INotificationType';
 
-const NotificationManager = () => {
-  return <></>;
+interface NotifyType {
+  type: string;
+  message: string;
+  title: string;
+  timeOut: number;
+  onClick: () => void;
+  priority: boolean;
+}
+
+const createUUID = () => {
+  const pattern = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+  return pattern.replace(/[xy]/g, c => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
+const Constants = {
+  CHANGE: 'change',
+  INFO: 'info',
+  SUCCESS: 'success',
+  WARNING: 'warning',
+  ERROR: 'error',
+};
+
+const NotificationManager: EventEmitter = () => {
+  const [listNotify, setListNotify] = useState<NotifyType[]>([]);
+
+  const create = (notify: NotifyType) => {
+    const defaultNotify = {
+      id: createUUID(),
+      type: 'info',
+      title: null,
+      message: null,
+      timeOut: 5000,
+    };
+    if (notify.priority) {
+      listNotify.unshift(Object.assign(defaultNotify, notify));
+    } else {
+      listNotify.push(Object.assign(defaultNotify, notify));
+    }
+    emitChange();
+  };
+
+  const emitChange = () => {
+    emit(Constants.CHANGE, this.listNotify);
+  };
 };
 
 export default NotificationManager;

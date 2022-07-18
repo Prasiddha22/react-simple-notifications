c65e6f94bdc24b2d33cbfb164831be1d3bc85144
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-07-18 07:29:41 +0545

    Code clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Notification } from './Notification.js';
 import { NotificationContainer } from './NotificationContainer';
-import { NotificationManager } from './NotificationManager';
+import NotificationManager from './NotificationManager';
 
 export { Notification, NotificationContainer, NotificationManager };
 export default Notification;
